88a7029039c397a2363d202f4fdff2941b50b787
nguyenchuen2003 <106584958+nguyenchuen2003@users.noreply.github.com>
2023-01-07 22:10:15 +0700

    update

---
+++ section2/PART1.js
@@ -0,0 +1,16 @@
+// PART 1 
+
+const massMark = 78 ; 
+const heightMark = 1.69 ; 
+const massJohn = 85 ; 
+const heightjohn = 1.76 ; 
+
+const BMIMark = massMark / heightMark **2 ; 
+const BMIJohn = massJohn / (heightjohn*heightjohn) ; 
+const markHigherBMI = BMIJohn > BMIMark ; 
+
+
+console.log (BMIJohn,BMIMark,markHigherBMI);
+
+// END PART 1 
+
